81f608fd8fc023b48b015e1e7a75a3eb593a2629
Int-aman <aman.yadav3@swiggy.in>
2022-07-18 18:21:28 +0530

    Created following classes 1.Pencil 2.Box 3.Point 4.Line 5.Whiteboard 6.GeometricShapes

---
+++ src/main/java/objects/Box.java
@@ -0,0 +1,22 @@
+public class Box {
+
+    private double height;
+    private double breadth;
+    private double length;
+    private boolean isFull;
+    private boolean isEmpty;
+    private String material;
+
+    public Box(double height, double breadth, double length, String material){
+        this.height = height;
+        this.breadth = breadth;
+        this.length = length;
+        this.material = material;
+    }
+
+    public double showVolume(){
+        double volume = height*length*breadth;
+        System.out.println(volume);
+    }
+
+}
---
+++ src/main/java/objects/Pencil.java
@@ -0,0 +1,20 @@
+public class Pencil {
+    private int length;
+    private String Colour;
+    private String type;
+
+    public Pencil(int length, String Colour, String type){
+        this.length = length;
+        this.Colour = Colour;
+        this.type = type;
+    }
+
+    public void write(){
+        System.out.println("Pencil is being used for writing.");
+    }
+
+    public void sharpen(){
+        System.out.println("Pencil is sharpenend.");
+        length -= 1;
+    }
+}
